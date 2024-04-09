bae17e8d4c06beba2afe01d1fdbefd2a74397062
Dmitry Vavaev <dmva1120@netcracker.com>
2024-04-09 12:29:26 +0300

    limits

--- src/routes/items.py
+++ src/routes/items.py
@@ -1,8 +1,18 @@
 from app import app
+from flask import request
 from requests import get
 
-@app.route("/items")
+@app.route("/items/")
 def get_items():
-    resp = get('https://jsonplaceholder.typicode.com/comments')
-
-    return resp.json()
+    """
+    in this api we can get up-to 50000 entries as a response
+    remote service data format:
+        [offset: 0, data: [{'name': 'name1', 'type': 'type1', }]]
+    """
+    offset = int(request.args.get('offset', 0))
+    qty = int(request.args.get('qty', 0))
+    resp = get('https://jsonplaceholder.typicode.com/comments').json()
+    data = []
+    for j in range(qty):
+        data.append(resp[offset+j])
+    return data
